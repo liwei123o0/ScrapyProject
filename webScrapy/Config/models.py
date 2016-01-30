5f64062c4df5a422f7c2829176de0366a559bb67
liwei <877129310@qq.com>
2016-01-31 01:46:27 +0800

    2016-1-31.02

--- webScrapy/Config/models.py
+++ webScrapy/Config/models.py
@@ -3,5 +3,19 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
-# Create your models here.
+@python_2_unicode_compatible
+class PdlistURL(models.Model):
+
+    name = models.CharField(u'网站名',max_length=255)
+    url  = models.CharField(u'URL',max_length=255)
+    domains  = models.CharField(u'domains',max_length=255)
+    author  =   models.CharField(u'模板作者',max_length=255)
+    title_xpath   =   models.CharField(u'标题检索表达式',max_length=255)
+    title_regex   =   models.CharField(u'标题正则',max_length=255)
+    url_xpath     =   models.CharField(u'URL检索表达式',max_length=255)
+    url_regex     =   models.CharField(u'URL正则',max_length=255)
+
+    def __str__(self):
+        return self.name
--- webScrapy/Config/views.py
+++ webScrapy/Config/views.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 #! /usr/bin/env python
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+from .models import *
+
 
 def index(request):
     return render(request,'index.html')
