eec252c54a93e073e604293a69b93b7f2dbe3e72
liwei <877129310@qq.com>
2016-02-03 16:32:31 +0800

    2016-2-3.01

--- webScrapy/webScrapy/settings.py
+++ webScrapy/webScrapy/settings.py
@@ -79,8 +79,14 @@ WSGI_APPLICATION = 'webScrapy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'scrapyproject',
+        'USER':'root',
+        'PASSWORD':'root',
+        'HOST':'127.0.0.1',
+        'PORT':'3306',
     }
 }
 
